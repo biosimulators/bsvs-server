da2b32436be521062feb71251c482b94d1eb5aba
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-12-23 15:24:06 -0500

    update deploy scripts

---
+++ assets/docker/scripts/deploy_all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+# build api
+./assets/docker/scripts/build_microservice.sh gateway
+
+# deploy api
+./assets/docker/scripts/deploy_microservice.sh gateway
+
+# build worker
+./assets/docker/scripts/build_microservice.sh worker
+
+# deploy worker
+./assets/docker/scripts/deploy_microservice.sh worker
