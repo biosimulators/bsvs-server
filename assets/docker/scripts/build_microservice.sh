43af63ce1df38444e33d54643a4e2e931baac97c
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-12-23 19:01:55 -0500

    update build script

--- assets/docker/scripts/build_microservice.sh
+++ assets/docker/scripts/build_microservice.sh
@@ -6,6 +6,7 @@ service="$1"
 
 if [ "$service" != "" ]; then
   echo "Building specified library: $service"
+  rm -rf "$service"/__pycache__
   docker compose build "$service" --no-cache
   echo "$service built!"
 fi
