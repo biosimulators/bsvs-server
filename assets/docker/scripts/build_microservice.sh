2a0e5981b46bd3dd6863e3990e0817d9d9403577
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-12-23 23:20:14 -0500

    update script

--- assets/docker/scripts/build_microservice.sh
+++ assets/docker/scripts/build_microservice.sh
@@ -3,10 +3,11 @@
 set -e
 
 service="$1"
+no_cache="$2"  # pass as --no-cache
 
-if [ "$service" != "" ]; then
-  echo "Building specified library: $service"
-  rm -rf "$service"/__pycache__
-  docker compose build "$service" --no-cache
-  echo "$service built!"
-fi
+echo "Building specified library: $service"
+rm -rf "$service"/__pycache__
+
+docker compose build "$service" "$no_cache"
+
+echo "$service built!"
