30832ce501a03133351b38ee330fb621695d243c
jcschaff <schaff.jim@gmail.com>
2025-01-17 09:46:03 -0500

    rename OpenAPI name from bsvs-server to biosim-server

--- biosim_server/api/main.py
+++ biosim_server/api/main.py
@@ -47,7 +47,7 @@ else:
 
 MONGO_URI = os.getenv("MONGO_URI")
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-APP_TITLE = "bsvs-server"
+APP_TITLE = "biosim-server"
 APP_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:4200',
